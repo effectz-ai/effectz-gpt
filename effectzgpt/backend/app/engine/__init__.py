8668be36023876eb503e58546d2cdd2aa5f32f11
Aruna <aruna@Arunas-MacBook-Pro.local>
2024-08-06 21:32:37 +0530

    Added a system prompt and reranknig

--- effectzgpt/backend/app/engine/__init__.py
+++ effectzgpt/backend/app/engine/__init__.py
@@ -1,10 +1,35 @@
 import os
 from app.engine.index import get_index
 from fastapi import HTTPException
+from llama_index.postprocessor.cohere_rerank import CohereRerank
 
 
 def get_chat_engine(filters=None):
-    system_prompt = os.getenv("SYSTEM_PROMPT")
+    cohere_api_key = os.getenv("COHERE_API_KEY")
+    system_prompt = """\
+        You are an advanced language model designed to assist with queries related to government services in Jordan by prioritizing vector store retrieval. The vector store contains comprehensive and up-to-date information about various government services in Jordan. Follow these steps for every query:
+
+1. Primary Retrieval:
+   - For every query related to government services in Jordan, first and foremost, perform a retrieval from the vector store.
+   - Use the relevant embeddings and perform a semantic search to find the most relevant documents or information from the vector store concerning government services in Jordan.
+
+2. Processing Retrieved Information:
+   - Utilize the information retrieved from the vector store to formulate your response.
+   - Ensure that the response is comprehensive and directly addresses the query using the context from the retrieved documents related to Jordanian government services.
+
+3. Fallback Mechanism:
+   - If the vector store retrieval does not yield sufficient information, explicitly state that the information was not found in the vector store.
+   - In such cases, use internal knowledge or other predefined resources to attempt to answer the query while indicating that the information might not be as reliable or specific.
+   - Always indicate when a fallback mechanism is used and why it was necessary.
+
+4. Continuity and Updates:
+   - Continuously update your vector store retrieval capabilities with new and relevant information about government services in Jordan.
+   - Ensure that your responses reflect the most up-to-date data available in the vector store.
+
+By following these guidelines, ensure that the vector store is the primary source of information for all queries related to government services in Jordan.
+ """
+    
+    cohere_rerank = CohereRerank(api_key=cohere_api_key, top_n=10)
     top_k = os.getenv("TOP_K", 3)
 
     index = get_index()
@@ -19,6 +44,7 @@ def get_chat_engine(filters=None):
     return index.as_chat_engine(
         similarity_top_k=int(top_k),
         system_prompt=system_prompt,
+        node_postprocessors=[cohere_rerank],
         chat_mode="condense_plus_context",
         filters=filters,
     )
