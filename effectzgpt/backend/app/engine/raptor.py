093c9fbe274bddc3c9dc8c8387c86719fe4d8ec3
Isuru Jinasena <iujinasena@gmail.com>
2024-09-24 10:53:05 +0530

    Added RAPTOR ingestion

---
+++ effectzgpt/backend/app/engine/raptor.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import logging
+import os
+
+from llama_index.packs.raptor import RaptorPack, RaptorRetriever
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.settings import Settings
+from llama_index.core.extractors import TitleExtractor
+from llama_index.core.ingestion import IngestionPipeline
+from app.settings import init_settings
+from app.engine.vectordb import get_vector_store
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+init_settings()
+
+vector_store = get_vector_store()
+
+def raptor_ingestion(path):
+    logger.info("Generate RaptorPack for the provided data")
+    data_dir = os.path.abspath(path)
+    documents = SimpleDirectoryReader(input_dir=data_dir).load_data()
+
+    pipeline = IngestionPipeline(
+        transformations=[
+            SentenceSplitter(chunk_size=100, chunk_overlap=0),
+            TitleExtractor(),
+            Settings.embed_model,
+        ]
+    )
+
+    pack = RaptorPack(documents, llm=Settings.llm, embed_model=Settings.embed_model, vector_store=vector_store)
+
+    pipeline.run(pack)
+
+    logger.info("Generation completed!")
+
