0dd8f8cebc232aa0341c38f4750710f7ca82390c
navindu-sachintha <navindu.social@gmail.com>
2024-09-28 17:33:02 +0530

    Added RAG app admin panel

--- effectzgpt/admin/src/app/layout.tsx
+++ effectzgpt/admin/src/app/layout.tsx
@@ -1,16 +1,18 @@
+"use client"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeContext } from "@/context";
-import { NavBar } from "@/components";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "EffectzGPT",
-  description: "Your Trusted AI Partner",
-};
+// export const metadata: Metadata = {
+//   title: "EffectzGPT",
+//   description: "Your Trusted AI Partner",
+// };
 
+const queryClient = new QueryClient();
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,8 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeContext>
-          <NavBar />
-          {children}
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
         </ThemeContext>
       </body>
     </html>
