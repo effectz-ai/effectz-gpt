b4bca92ccef7397cebc1948786bef2147c45e97a
navindu-sachintha <navindu.social@gmail.com>
2024-09-28 13:02:02 +0530

    Migrated ragapp pages router to App router

--- effectzgpt/frontend/app/admin/layout.tsx
+++ effectzgpt/frontend/app/admin/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { ThemeContext } from "@/context";
-import { NavBar } from "@/components/admin";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,8 @@ export const metadata: Metadata = {
   title: "EffectzGPT",
   description: "Your Trusted AI Partner",
 };
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -20,8 +23,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeContext>
-          <NavBar />
-          {children}
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
         </ThemeContext>
       </body>
     </html>
