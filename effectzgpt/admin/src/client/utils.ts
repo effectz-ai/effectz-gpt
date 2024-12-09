3daa6cc2a74e06b4b20496e8a736d6cd230e7667
navindu-sachintha <navindu.social@gmail.com>
2024-12-09 10:45:03 +0530

    ♻️ Updated refactor

--- effectzgpt/admin/src/client/utils.ts
+++
@@ -1,7 +0,0 @@
-export function getBaseURL(): string {
-  // If we are in development, use the local backend endpoint
-  if (process.env.NEXT_PUBLIC_ENVIRONMENT === "dev") {
-    return "http://localhost:5001";
-  }
-  return typeof window !== "undefined" ? window.location.origin : "";
-}
