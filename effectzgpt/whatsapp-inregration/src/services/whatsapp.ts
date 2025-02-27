19a16cd12f460afb8259185292de9c065aff5844
navindu-sachintha <navindu.social@gmail.com>
2025-02-27 16:03:22 +0530

    Add WhatsappService class to handle sending text messages

--- effectzgpt/whatsapp-inregration/src/services/whatsapp.ts
+++ effectzgpt/whatsapp-inregration/src/services/whatsapp.ts
@@ -1,14 +1,29 @@
 import 'dotenv/config';
 
 import {WhatsAppAPI} from 'whatsapp-api-js'
+import { Text } from 'whatsapp-api-js/messages';
 
 const TOKEN = process.env.WHATSAPP_ACCESS_TOKEN;
 const APP_SECRET = process.env.WHATSAPP_APP_SECRET;
+const PHONE_ID = process.env.WHATSAPP_PHONE_NUMBER_ID;
 
 export const WhatsappClient = new WhatsAppAPI({
     token: TOKEN!,
     appSecret:APP_SECRET!
 })
 
-
+export class WhatsappService {
+    async sendText(to:string, text:string){
+        if(PHONE_ID === undefined){
+            throw new Error('Phone number ID is not defined');
+        }
+        try {
+            const textMsg = new Text(text)
+            return await WhatsappClient.sendMessage(PHONE_ID,to, textMsg)
+        } catch (error) {
+            console.error(error)
+            throw new Error('Failed to send message')
+        }
+    }
+}
 
