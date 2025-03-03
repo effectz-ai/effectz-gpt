08b3b6a7dad0f9b2b05f9a1e43747726e5f879fa
navindu-sachintha <navindu.social@gmail.com>
2025-03-03 16:05:15 +0530

    Remove deprecated webhook handling and related files; update .gitignore to exclude build artifacts

--- effectzgpt/whatsapp-inregration/build/routes/webhookRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const webhookControllers_1 = require("../controllers/webhookControllers");
-const webHookRouter = (0, express_1.Router)();
-webHookRouter.get('/', webhookControllers_1.verifyWebhook);
-webHookRouter.post('/', webhookControllers_1.handleWebhook);
-exports.default = webHookRouter;
